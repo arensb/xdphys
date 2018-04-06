54fb53854c252190a0fe7ecc328b82edcdf3792a
Andrew Arensburger <arensb@umd.edu>
2018-04-06 07:57:31 -0400

    Add setup scripts. Written by Kevin Hildebrand, I think.

---
+++ setup_environment.csh
@@ -0,0 +1,8 @@
+setenv DAQPROG /export/software/barnowl/bin/tdtproc
+setenv DAQ_AP2_PORT apb
+setenv XDPHYSDIR /export/software/barnowl/lib/xdphys
+setenv XAPPLRESDIR /export/software/barnowl/lib/X11/app-defaults
+setenv LD_LIBRARY_PATH /export/software/barnowl/lib
+setenv PATH /export/software/barnowl/bin/xdphys:/export/software/barnowl/bin:$PATH
+
+
---
+++ setup_environment.sh
@@ -0,0 +1,8 @@
+DAQPROG=/export/software/barnowl/bin/tdtproc; export DAQPROG
+DAQ_AP2_PORT=apb; export DAQ_AP2_PORT
+XDPHYSDIR=/export/software/barnowl/lib/xdphys; export XDPHYSDIR
+XAPPLRESDIR=/export/software/barnowl/lib/X11/app-defaults; export XAPPLRESDIR
+LD_LIBRARY_PATH=/export/software/barnowl/lib; export LD_LIBRARY_PATH
+PATH=/export/software/barnowl/bin/xdphys:/export/software/barnowl/bin:$PATH; export PATH
+
+
